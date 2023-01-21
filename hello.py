6e8327cf5b6bde89d5f8aa5412e07c6806d868eb
fabaneon <fabaneon@gmail.com>
2023-01-21 02:36:24 +0000

    2023-01-21 실습 환경 구축 완료 + 커널: 우분투 + 서버 프로그램 : Apache 2 + CGI <-> 아파치  = 파이썬으로 코드 작성
    기존 /var/www/html 인 Root Document를
    /workspace/Python_Study로 변경.
    
    즉, 당장 Github Server로는 실습예제 확인이불가능하다.
    
    확인하고 싶다면 해당 파일을 다운로드 받은 뒤
    열람 pc에 Apache2와 nano 에디터를 다운받고
    apache2 파일의 apache2.conf 속 기본 Dir를
    본 레파지토리 클로닝 파일로 변경한 후
    
    apache2/sites-enabled/000-default.conf 내용에
    
             <Directory /workspace/Python_Study>
    
                    AddHandler cgi-script .py
                    Options ExecCGI
                    Order allow, deny
                    Allow from all
            </Directory>
    를 추가해 py 파일을 CGI로 읽어들일 수 있게끔 해준다.
    
    PY 파일 작성할땐 hear 코드를 추가해주는것도 잊지 말것
    #!/usr/bin/python3 이런식으로
    html의
    <!DOCTYPE html> 과 비슷한 성격이다.

---
+++ hello.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+print("Content-Type:text/html")
+print()
+print('''<!DOCTYPE html>
+<html>
+    <head>
+        <title>Web Dev with Python</title>
+        <meta charset="utf-8">
+        
+    </head>
+    <body>
+        <h1>
+            Hello WEB Python
+        </h1>
+        <p>
+            can't use korean
+        </p>
+    </body>
+</html>
+''')
--- index.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/pyhton
-print("content-type:text/html; carset=UTF-8\n")
-print("hello")
