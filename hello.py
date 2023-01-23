da5563b2465e74c9ae8eebfade4a748c21f63dd1
fabaneon <fabaneon@gmail.com>
2023-01-23 03:06:19 +0000

    2023-01-23 update 기능 구현중
    음.. 별도의 hello.py 문서를 만들지 않고 싱글페이지로 업데이트를 구현하려하고있다.
    
    보통의 기초 예제들은 hello_update.py 같은, 비슷한 양식의 문서를 만들고 페이지에 작성된 기능만 변경하여 사용하는데
    이러면 중복이 좀 많이 생겨서 불 ㅡ 편 하다.
    
    state함수처럼 사용할만한걸 찾아볼까 싶다가도
    그러면 코드가 Sexy 하지 못하기때문에 간단한 방법을 찾아보는중.
    
    일단 밥먹고 와서 고민해볼까 한다.

--- hello.py
+++ hello.py
@@ -4,9 +4,10 @@ print()
 
 import cgi, os
 
-
+# os 모듈 / 운영체제에서 제공되는 여러 기능
 navlist = os.listdir('hello_data')
 navbutton = ''
+
 for links in navlist:
     navbutton = navbutton + '<li><a href="hello.py?id={name}">{name}</a></li>'.format(name=links)
 
@@ -14,6 +15,10 @@ form = cgi.FieldStorage()
 
 if 'id' in form:
     pageId = form["id"].value
+    option = '''
+            <li><a href='hello.py?id={}'>update</a>
+            <a href='hello.py'>back</a>
+    '''.format(pageId)
     if pageId == 'create':
         article = '''
         <form action="create_function.py" method="post">
@@ -29,12 +34,39 @@ if 'id' in form:
         </form>
         
         '''
-    else:
+    elif 'update' in form:
+        print('updatemode')
+        file_article = open('hello_data/'+pageId, 'r').read()
+        article = '''
+        <form action="create_function.py" method="post">
+               <input name="pageId" type="hidden" value={form_default_title}/>
+            <p>
+               <input required name="title" type="text" value="{form_defualt_title}"/>
+            </p>
+            <p>   
+                <textarea required name="article" row="4">
+                    {form_default_article}
+                </textarea>
+            </p>
+            <p>   
+                <input type="submit"/>
+            </p>
+        </form>
         
+        
+        
+        '''.format(
+                    form_default_title=pageId,
+                  form_default_article=file_article
+                  )
+    else:
+        print(pageId)
         article = open('hello_data/'+pageId, 'r').read()
         
     
 else:
+    print('nothing')
+    option = "<a href='hello.py?id=create'>create</a>"
     pageId = "welcome"
     article = "Check out the other article to use navigator link at above."
     
@@ -53,7 +85,7 @@ print('''<!DOCTYPE html>
             {nav}
         </ol>
         <p>
-            <a href='hello.py?id=create'>create</a>
+            {optionbutton}
             <h2>
                 {title}
             </h2>
@@ -66,7 +98,7 @@ print('''<!DOCTYPE html>
 '''.format(
             title=pageId,
            article=article,
-           nav=navbutton
-          
+           nav=navbutton,
+          optionbutton=option
           ))
 
