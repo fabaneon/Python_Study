c23681194cc07720a73325b05b789f158a01dbc2
fabaneon <fabaneon@gmail.com>
2023-01-24 09:19:53 +0000

    2023-01-24 Django 학습
    음.. 일단 여기에다 장고를 설치하긴 했는데.
    
    기존 아파치 서버때문에 중복이 생기는것 같기도..?
    
    일단 컨테이너를 하나 더 만들던지
    
    아니면 토글처럼 하나를 껐다가 다른걸 켰다가 하는 방법을 좀 찾아봐야겠다.

---
+++ python_django/python_web/urls.py
@@ -0,0 +1,25 @@
+#사용자 접속 path에 따라 요청을 어떻게 처리할것인가를 지정하는 routing 파일
+
+"""python_web URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from .views import HomeView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',HomeView.as_view(), name='home'),
+]
